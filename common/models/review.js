c2585f114ef0a373a0e96dfa0bd3c01666d47be0
Simon Ho <simon@strongloop.com>
2015-01-20 15:08:41 -0800

    Define a remote hook

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,8 @@
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
+    req.body.date = Date.now();
+    req.body.publisherId = req.accessToken.userId;
+    next();
+  });
 };
