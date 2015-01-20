9a0f091889dab1a724f8e24506ac9d6b115a5df4
Simon Ho <simon@strongloop.com>
2015-01-20 15:16:36 -0800

    Create an AngularJS client - No `lb-services.js`

---
+++ client/js/controllers/auth.js
@@ -0,0 +1,37 @@
+angular
+  .module('app')
+  .controller('AuthLoginController', ['$scope', 'AuthService', '$state',
+      function($scope, AuthService, $state) {
+    $scope.user = {
+      email: 'foo@bar.com',
+      password: 'foobar'
+    };
+
+    $scope.login = function() {
+      AuthService.login($scope.user.email, $scope.user.password)
+        .then(function() {
+          $state.go('add-review');
+        });
+    };
+  }])
+  .controller('AuthLogoutController', ['$scope', 'AuthService', '$state',
+      function($scope, AuthService, $state) {
+    AuthService.logout()
+      .then(function() {
+        $state.go('all-reviews');
+      });
+  }])
+  .controller('SignUpController', ['$scope', 'AuthService', '$state',
+      function($scope, AuthService, $state) {
+    $scope.user = {
+      email: 'baz@qux.com',
+      password: 'bazqux'
+    };
+
+    $scope.register = function() {
+      AuthService.register($scope.user.email, $scope.user.password)
+        .then(function() {
+          $state.transitionTo('sign-up-success');
+        });
+    };
+  }]);
