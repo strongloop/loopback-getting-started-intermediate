f852c73a577749cbdfa1acc775277c6a2b0eefec
Simon Ho <simon@strongloop.com>
2015-02-02 09:30:16 -0800

    Simply non-dump scripts

--- bin/demo.sql
+++ bin/demo.sql
@@ -1,7 +1,7 @@
--- Drop `CoffeeShop` table if it exists
+-- Drop the `CoffeeShop` table if it exists
 DROP TABLE IF EXISTS `CoffeeShop`;
 
--- Create a new `CoffeeShop` table
+-- Create the `CoffeeShop` table
 CREATE TABLE `CoffeeShop` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) DEFAULT NULL,
@@ -9,5 +9,5 @@ CREATE TABLE `CoffeeShop` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Insert values into the new `CoffeeShop` table
+-- Insert data the `CoffeeShop` table
 INSERT INTO `CoffeeShop` VALUES (1,'Bel Cafe','Vancouver'),(2,'Three Bees Coffee House','San Mateo'),(3,'Caffe Artigiano','Vancouver');
