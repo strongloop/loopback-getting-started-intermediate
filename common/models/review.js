d2314f32782bca708e994af0f2647ea8d894957d
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 12:17:02 -0700

    update copyright notices and license

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-getting-started-intermediate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 module.exports = function(Review) {
   Review.beforeRemote('create', function(context, user, next) {
     context.args.data.date = Date.now();
--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: loopback-getting-started-intermediate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var async = require('async');
 
 module.exports = function(app) {
