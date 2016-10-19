eeeba05590d758cdbbe9e0972acb100e972cb8d8
Simon Ho <superkhau@gmail.com>
2016-10-18 22:32:42 -0700

    Persist login state after refresh (#27)
    After login also redirect back to state that was requested before
    login. This helps in case of bookmarking url’s of states behind login

--- client/js/controllers/auth.js
+++ client/js/controllers/auth.js
@@ -15,6 +15,21 @@ angular
     $scope.login = function() {
       AuthService.login($scope.user.email, $scope.user.password)
         .then(function() {
+
+          // return to saved returnTo state before redirection to login
+          if ($scope.returnTo && $scope.returnTo.state) {
+            $state.go(
+              $scope.returnTo.state.name,
+              $scope.returnTo.params
+            );
+            // maintain the inherited rootscope variable returnTo
+            // but make the returnTo state of it null,
+            // so it can be used again after a new login.
+            $scope.returnTo.state  = null;
+            $scope.returnTo.params = null;
+            return;
+          }
+          // or go to the default state after login
           $state.go('add-review');
         });
     };
