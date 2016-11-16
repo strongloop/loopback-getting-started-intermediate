96905af249f0ce4d208cfe5f9ecf295bcf289073
Amir Jafarian <amirali.jfr61@gmail.com>
2016-11-16 17:22:58 -0500

    Define a remote hook

--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', function(context, user, next) {
+    context.args.data.date = Date.now();
+    context.args.data.publisherId = context.req.accessToken.userId;
+    next();
+  });
 };
