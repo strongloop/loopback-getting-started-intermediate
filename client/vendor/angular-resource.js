6e6b10146446d5fdb82fdb6b8ed0e5b2efea0954
Agnes Lin <agneslin.lin@ibm.com>
2019-05-08 10:25:11 -0400

    added MIT license into

--- client/vendor/angular-resource.js
+++ client/vendor/angular-resource.js
@@ -1,5 +1,7 @@
 // Copyright IBM Corp. 2015,2016. All Rights Reserved.
 // Node module: loopback-getting-started-intermediate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 
 /**
  * @license AngularJS v1.3.2
